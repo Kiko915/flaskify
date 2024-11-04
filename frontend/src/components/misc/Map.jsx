2a15bb7e188469be19942465295d89110f74461d
kikodevelops <kikoneilmistica@gmail.com>
2024-11-04 15:55:43 +0800

    Added react-confetti

--- frontend/src/components/misc/Map.jsx
+++ frontend/src/components/misc/Map.jsx
@@ -4,8 +4,6 @@ import { useState } from "react"
 export default function FMap() {
     const [position, setPosition] = useState([50.879, 4.6997])
 
-
-
   return (
     <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11} onClick={(e) => setPosition(e.latLng)}>
         <ZoomControl />
--- frontend/src/pages/user/profile/Profile.jsx
+++ frontend/src/pages/user/profile/Profile.jsx
@@ -1,7 +1,10 @@
+import UserCard from "../../../components/user/UserCard";
 
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <UserCard title="My Profile" short_description="Manage your account details" >
+      hey test
+    </UserCard>
   )
 }
